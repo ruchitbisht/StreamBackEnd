f6fc885a35f8e6d0368dcedeb6527a54c43eb6ed
Ruchit Bisht <ruchit@pratikshat.com>
2022-12-19 16:34:14 +0530

    done

---
+++ src/main/java/com/stream/controller/RegistrationController.java
@@ -0,0 +1,27 @@
+package com.stream.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.stream.entity.Registration;
+import com.stream.service.RegistrationService;
+
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/signUp")
+public class RegistrationController{
+
+    @Autowired
+    RegistrationService registraionService;
+
+    @PostMapping()
+    public void registereUser(@RequestBody Registration reg){
+
+        registraionService.registereUser(reg);
+
+    }
+}
